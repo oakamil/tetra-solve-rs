9a28e29d8786fa38761bc875830d42780bb1eeb0
oakamil <oakamil@gmail.com>
2026-03-12 17:59:31 -0700

    Update license text

--- tetra3/src/tetra3.rs
+++ tetra3/src/tetra3.rs
@@ -1,5 +1,75 @@
-// Copyright (c) 2026 Omair Kamil oakamil@gmail.com
-// See LICENSE file in root directory for license terms.
+// Copyright (c) 2026 Omair Kamil <oakamil@gmail.com>
+// 
+// This file is a derivative work - a port to Rust with heavy performance
+// optimizations from `tetra3.py` of the cedar-solve project. The original code
+// is licensed under the Apache License, Version 2.0.
+//
+// Licensed under the Functional Source License, Version 1.1, 
+// [MIT / Apache 2.0] Future License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of the
+// License at
+//
+//     https://fsl.software/FSL-1.1-[MIT / ALv2]
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+//
+// Cedar Solve license:
+//    Copyright 2023 Steven Rosenthal smr@dt3.org
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        https://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+//
+// tetra3 license:
+//    Copyright 2019 the European Space Agency
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        https://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+//
+// Original Tetra license notice:
+//    Copyright (c) 2016 brownj4
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
 
 use kiddo::{KdTree, SquaredEuclidean};
 use nalgebra::{DMatrix, DVector, Matrix3, SVD};
